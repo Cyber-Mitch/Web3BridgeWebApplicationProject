8d72f01505b93f8b72e52296eeca703227f93553
mitch <mitch@kali>
2022-06-04 16:30:18 +0100

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-
-  
-function validateFormValue() {
-    let inputName = document.getElementById("name").value;
-    let option = document.getElementById("tiers").value;
+function CalInterest() {
+    let inputName = document.querySelector('input[type=text]').value;
+    let option = document.querySelector('select#tiers').value;
     let user = [];
     let userDetails = {name:inputName, option};
     user.push(userDetails);
     localStorage.setItem(inputName,JSON.stringify(user));
-
-    alert(option)
+    
+    if(option == 10000){
+        document.getElementById("p1").innerHTML = "Your Interest will be " + (option/100) * 7 ;
+        document.getElementById("p2").innerHTML = "Your Weekly Total will be " + (option/100) * (100+7) ;
+    }
+    else if(option == 20000){
+        document.getElementById("p1").innerHTML = "Your Interest will be " + (option/100) * 12 ;
+        document.getElementById("p2").innerHTML = "Your Weekly Total will be " + (option/100) * (100+7);
+    }
+    else if(option == 30000){
+        document.getElementById("p1").innerHTML = "Your Interest will be " + (option/100) * 25 ;
+        document.getElementById("p2").innerHTML = "Your Weekly Total will be " + (option/100) * (100+7);
+    }
 }
